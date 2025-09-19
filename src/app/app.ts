035743be813c66710cb5a4728dc58177d2f3d7e2
TheEfox <clan.b3ny@gmail.com>
2025-09-19 19:33:35 +0200

    Fixing tab spaces in code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 
 import { Home } from "./components/home/home";
--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -3,22 +3,22 @@ import { CommonModule } from '@angular/common';
 import { Footer } from "../footer/footer"; // Necesario para *ngFor
 
 @Component({
-  selector: 'app-about',
-  standalone: true,
-  imports: [CommonModule, Footer],
-  templateUrl: './about.html',
-  styleUrls: ['./about.css']
+    selector: 'app-about',
+    standalone: true,
+    imports: [CommonModule, Footer],
+    templateUrl: './about.html',
+    styleUrls: ['./about.css']
 })
 export class About {
 // Array de imágenes
-  projects = [
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg'
-  ];
+    projects = [
+        'https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg',
+        'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg',
+        'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg',
+        'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg',
+        'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg',
+        'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg',
+        'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg',
+        'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg'
+    ];
 }
